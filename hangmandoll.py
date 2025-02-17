a2b4b0ca06a9de275754eb035cd8a51dd8e2d562
eglasb <98265972+eglasb@users.noreply.github.com>
2025-02-17 06:56:33 -0300

    Update hangmandoll numbers

--- hangmandoll.py
+++ hangmandoll.py
@@ -2,12 +2,12 @@ class HangmanDoll:
     def __init__(self) -> None:
         self.drawings: dict = {
             6: "desenho com 6 vidas",
-            5: "desenho com 6 vidas",
-            4: "desenho com 6 vidas",
-            3: "desenho com 6 vidas",
-            2: "desenho com 6 vidas",
-            1: "desenho com 6 vidas",
-            0: "desenho com 6 vidas",
+            5: "desenho com 5 vidas",
+            4: "desenho com 4 vidas",
+            3: "desenho com 3 vidas",
+            2: "desenho com 2 vidas",
+            1: "desenho com 1 vidas",
+            0: "desenho com 0 vidas",
         }
 
     def draw_hangman(self, lifes: int):
